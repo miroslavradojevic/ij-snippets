a1b64567f62277ec92ee8cecdf7d7eb70240719b
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2017-07-24 19:14:17 +0200

    ImageJ plugin frame for sampling circles.

---
+++ src/com/braincadet/SampleCirc.java
@@ -0,0 +1,17 @@
+package com.braincadet;
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+/**
+ * Created by Miroslav on 23-Jul-17.
+ */
+public class SampleCirc implements PlugIn {
+    public void run(String s) {
+
+        IJ.log("testing plugin...");
+
+
+
+    }
+}
