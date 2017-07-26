b77d33f4da65181179b050f046395f8e54ff8256
Miroslav Radojevic <10250153@bd.com>
2017-07-26 17:02:04 +0200

    Added images, removed binaries and couple of the lines of the code.

--- src/com/braincadet/SampleCirc.java
+++ src/com/braincadet/SampleCirc.java
@@ -7,11 +7,34 @@ import ij.plugin.PlugIn;
  * Created by Miroslav on 23-Jul-17.
  */
 public class SampleCirc implements PlugIn {
+
+    float r;
+    float rMin, rMax;
+    float d = 3f;
+    float samplingStep = 2f;
+
+    int imageWidth;
+    int imageHeight;
+
     public void run(String s) {
 
-        IJ.log("testing plugin...");
+        IJ.log("read image...");
+
+        // Random sample the circles (x,y,r,d)
+        rMin = 0.1f * imageWidth;
+        rMax = ;
+
+        // Generate random ovals
 
 
 
     }
+
+    private class cRoi {
+        public float x;
+        public float y;
+        public float r;
+        public float d;
+    }
+
 }
