3bbf17ea239cdcb43f56e75d8ae2847c6f757a69
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2018-03-22 07:19:45 +0100

    patch sampling using systematic resampling of the annotation initiated

--- src/com/braincadet/bacannot/PatchExtractor.java
+++ src/com/braincadet/bacannot/PatchExtractor.java
@@ -1,80 +1,64 @@
 package com.braincadet.bacannot;
 
 import ij.IJ;
+import ij.ImagePlus;
 import ij.Prefs;
 import ij.gui.GenericDialog;
+import ij.gui.Overlay;
+import ij.gui.PointRoi;
 import ij.io.OpenDialog;
 import ij.plugin.PlugIn;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.Random;
 
 public class PatchExtractor implements PlugIn {
 
     String annotDir;
+    int R = 8;
+    int N = 10;
+    int gaussBlur = 12;
+
+    private  static final String PATCH_DIR_NAME = "patch";
 
     @Override
     public void run(String s) {
 
         GenericDialog gd = new GenericDialog("Extract patches");
-        gd.addStringField("annotdir",     Prefs.get("com.braincadet.bacannot.annotdir", annotDir), 30);
-
-//        gd.addStringField("th",         Prefs.get("com.braincadet.phd.th", th_csv), 10);
-//        gd.addStringField("no",         Prefs.get("com.braincadet.phd.no", no_csv), 20);
-//        gd.addStringField("ro",         Prefs.get("com.braincadet.phd.ro", ro_csv), 10);
-//        gd.addStringField("ni",         Prefs.get("com.braincadet.phd.ni", ni_csv), 10);
-//        gd.addStringField("step",       Prefs.get("com.braincadet.phd.step", step_csv), 10);
-//        gd.addStringField("kappa",      Prefs.get("com.braincadet.phd.kappa", kappa_csv), 10);
-//        gd.addStringField("ps",         Prefs.get("com.braincadet.phd.ps", pS_csv), 10);
-//        gd.addStringField("pd",         Prefs.get("com.braincadet.phd.pd", pD_csv), 10);
-//        gd.addStringField("krad",       Prefs.get("com.braincadet.phd.krad", krad_csv), 10);
-//        gd.addStringField("kc",         Prefs.get("com.braincadet.phd.kc", kc_csv), 10);
-//        gd.addNumericField("maxiter",   Prefs.get("com.braincadet.phd.maxiter", maxiter), 0, 5, "");
-//        gd.addStringField("maxepoch",   Prefs.get("com.braincadet.phd.maxepoch", Integer.toString(maxepoch)), 10);
-//        gd.addCheckbox("savemidres",    Prefs.get("com.braincadet.phd.savemidres", savemidres));
-
+        gd.addStringField("annotdir",     Prefs.get("com.braincadet.bacannot.annotdir", annotDir), 50);
+        gd.addNumericField("R",   Prefs.get("com.braincadet.bacannot.r", R), 0, 5, "");
+        gd.addNumericField("N",   Prefs.get("com.braincadet.bacannot.n", N), 0, 5, "");
         gd.showDialog();
         if (gd.wasCanceled()) return;
-
-        annotDir = gd.getNextString();
+        annotDir = gd.getNextString().replace("\"", "");
         Prefs.set("com.braincadet.bacannot.annotdir", annotDir);
 
-//        th_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.th", th_csv);
-//        no_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.no", no_csv);
-//        ro_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.ro", ro_csv);
-//        ni_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.ni", ni_csv);
-//        step_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.step", step_csv);
-//        kappa_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.kappa", kappa_csv);
-//        pS_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.ps", pS_csv);
-//        pD_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.pd", pD_csv);
-//        krad_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.krad", krad_csv);
-//        kc_csv = gd.getNextString();
-//        Prefs.set("com.braincadet.phd.kc", kc_csv);
-//        maxiter = (int) gd.getNextNumber();
-//        Prefs.set("com.braincadet.phd.maxiter", maxiter);
-//        String maxepoch_str = gd.getNextString();
-//        maxepoch = (maxepoch_str.equals("inf")) ? Integer.MAX_VALUE : Integer.valueOf(maxepoch_str);
-//        Prefs.set("com.braincadet.phd.maxepoch", maxepoch);
-//        savemidres = gd.getNextBoolean();
-//        Prefs.set("com.braincadet.phd.savemidres", savemidres);
+        R = (int) gd.getNextNumber();
+        Prefs.set("com.braincadet.bacannot.r", R);
+
+        N = (int) gd.getNextNumber();
+        Prefs.set("com.braincadet.bacannot.n", N);
 
         System.out.println("annotdir=" + annotDir);
+        System.out.println("R=" + R);
+        System.out.println("N=" + N);
+
         File fAnnotDir = new File(annotDir);
+
         if (fAnnotDir.exists() == false) {
-            System.out.println("Enetered annot directory does not exist!");
+            System.out.println("Enetered annot directory does not exist.");
             return;
         }
 
-        // read all available annotation images "**.tif"
+        if (fAnnotDir.isDirectory() == false) {
+            System.out.println("Enetered annot path is not a directory.");
+            return;
+        }
+
+        System.out.println("parent = " + fAnnotDir.getParent());
+
+        // read all available annotation images "*.tif"
         File [] files = fAnnotDir.listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
@@ -82,10 +66,110 @@ public class PatchExtractor implements PlugIn {
             }
         });
 
-        System.out.println("dound " + files.length + "annotations");
+        if (files.length == 0) {
+            System.out.println("No annotations were found.");
+            return;
+        }
+
+        System.out.println("found " + files.length + "annotations");
+
+
+        File directory = new File(fAnnotDir.getParent() + File.separator + PATCH_DIR_NAME + File.separator);
+
+        if (! directory.exists()){
+            directory.mkdir(); // use directory.mkdirs(); for subdirs
+        }
+        else {
+            if (directory.listFiles().length > 0) {
+                System.out.println("Patch directory contains files.");
+                return;
+            }
+        }
+
+
+
+
+        int countSamples = 1;
 
         for (File annotImgPath : files) {
+
             System.out.println(annotImgPath.getAbsolutePath());
+
+
+
+            // read image
+            ImagePlus annotImg = new ImagePlus(annotImgPath.getAbsolutePath());
+            if (annotImg.getType() == ImagePlus.GRAY8) {
+
+                // if 8bit and .tif consider it
+                // check extension too
+                // smooth
+                IJ.run(annotImg, "Gaussian Blur...", "sigma="+gaussBlur);
+
+                byte[] annotImgPixels = (byte[]) annotImg.getProcessor().getPixels();
+
+                System.out.println("annotImgPixels.length="+annotImgPixels.length);
+
+                float annotFuzzyMin = Float.POSITIVE_INFINITY, annotFuzzyMax = Float.NEGATIVE_INFINITY;
+                for (int i = 0; i < annotImgPixels.length; i++) {
+                    if ((annotImgPixels[i] & 0xff) < annotFuzzyMin) {
+                        annotFuzzyMin = annotImgPixels[i] & 0xff;
+                    }
+                    if ((annotImgPixels[i] & 0xff) > annotFuzzyMax) {
+                        annotFuzzyMax = annotImgPixels[i] & 0xff;
+                    }
+                }
+
+                System.out.println("annotFuzzyMin="+annotFuzzyMin);
+                System.out.println("annotFuzzyMax="+annotFuzzyMax);
+
+                if (annotFuzzyMax - annotFuzzyMin > Float.MIN_VALUE) {
+
+                    //cws compute
+                    float[] annotFuzzyCws = new float[annotImgPixels.length];
+
+                    for (int j = 0; j < annotFuzzyCws.length; j++) {
+                        annotFuzzyCws[j] = ((annotImgPixels[j]&0xff)-annotFuzzyMin)/(annotFuzzyMax-annotFuzzyMin); // min-max normalize
+                        annotFuzzyCws[j] = annotFuzzyCws[j] + ((j==0)? 0 : annotFuzzyCws[j-1]);
+                    }
+
+                    System.out.println("annotFuzzyCws.top()="+annotFuzzyCws[annotFuzzyCws.length-1]);
+
+                    int[] smp = sampleI(N, annotFuzzyCws);
+
+                    Overlay ov = new Overlay();
+                    for (int i = 0; i < smp.length; i++) {
+//                        System.out.println(smp[i] + " -- " + smp[i]%annotImg.getWidth() + " | " + smp[i]/annotImg.getWidth() );
+                        PointRoi p = new PointRoi(smp[i]%annotImg.getWidth(), smp[i]/annotImg.getWidth());
+//                        System.out.println(p.getXBase() + " " + p.getYBase());
+                        ov.add(p);
+                    }
+
+                    annotImg.setOverlay(ov);
+
+                    annotImg.show();
+
+                    System.out.println("OK");
+
+
+                }
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
 
             // sample positives
 
@@ -98,42 +182,67 @@ public class PatchExtractor implements PlugIn {
 
 
     }
+
+
+
+    private int[] sampleI(int nsamples, float[] csw) { // , int[][] tosample
+
+        int[] out = new int[nsamples];//[tosample[0].length];
+
+        float totalmass = csw[csw.length - 1];
+
+        // use systematic resampling, Beyond Kalman Filtering, Ristic et al.
+        int i = 0;
+
+        float u1 = (totalmass / (float) nsamples) * new Random().nextFloat();
+
+        for (int j = 0; j < nsamples; j++) {
+
+            float uj = u1 + j * (totalmass / (float) nsamples);
+
+            while (uj > csw[i]) {
+                i++;
+            }
+
+//            System.out.println("out["+j+"]="+out[j]);
+
+            out[j] = i;//tosample[i];//[k];[k]
+
+//            for (int k = 0; k < tosample[i].length; k++) {
+//                out[j][k] = tosample[i][k];
+//            }
+
+        }
+
+
+        return out;
+    }
+
+    // sampleXY
+
 }
 
 
 
-//    private int[][] sample(int nsamples, float[] csw, int[][] tosample) {
+
+
+
+
+
+//    private ArrayList<Integer> importsamp(ArrayList<Double> lcws, int n) {
+//        // systematic resampling, Beyond Kalman Filtering, Ristic et al.
+//        double totalmass = lcws.get(lcws.size() - 1);
+//        double u1 = (totalmass / (float) n) * new Random().nextDouble();
 //
-//        int[][] out = new int[nsamples][tosample[0].length];
-//
-//        float totalmass = csw[csw.length - 1];
-//
-//        // use systematic resampling
+//        ArrayList<Integer> out = new ArrayList<Integer>(n);
+//        out.clear();
 //        int i = 0;
-//
-//        float u1 = (totalmass / (float) nsamples) * new Random().nextFloat();
-//
-//        for (int j = 0; j < nsamples; j++) {
-//
-//            float uj = u1 + j * (totalmass / (float) nsamples);
-//
-//            while (uj > csw[i]) {
-//                i++;
-//            }
-//
-//            for (int k = 0; k < tosample[i].length; k++) {
-//                out[j][k] = tosample[i][k];
-//            }
-//
+//        for (int j = 0; j < n; j++) {
+//            double uj = u1 + j * (totalmass / (float) n);
+//            while (uj > lcws.get(i)) i++;
+//            out.add(i);
 //        }
-//
 //
 //        return out;
+//
 //    }
-
-
-//            for (int j = 0; j < stpr._pcws0.length; j++) {
-//        stpr._pcws0[j] = (tmax-tmin>Float.MIN_VALUE)?((stpr._pcws0[j]-tmin)/(tmax-tmin)):0;
-//        stpr._pcws0[j] = (float) (Math.pow(stpr._pcws0[j],weight_deg) * 1f);
-//        stpr._pcws0[j] = stpr._pcws0[j] + ((j==0)? 0 : stpr._pcws0[j-1]);
-//        }
