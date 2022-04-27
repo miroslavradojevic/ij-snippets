5deaf87d7507bab4e1386ed2c317cfef1cc05292
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2022-04-28 01:01:15 +0200

    Added bacteria annotator plugin and test plugin

---
+++ src/main/java/com/braincadet/annotbat/Test.java
@@ -0,0 +1,12 @@
+package com.braincadet.annotbat;
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+public class Test implements PlugIn {
+
+    public void run(String s) {
+        IJ.log("Test...");
+
+    }
+}
