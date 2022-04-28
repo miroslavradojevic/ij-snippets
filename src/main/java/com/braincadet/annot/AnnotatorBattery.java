10bfc1f86f052d3301e57b2388cbf1b767e33656
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2022-04-28 10:10:09 +0200

    Added one more plugin for battery annotation

---
+++ src/main/java/com/braincadet/annot/AnnotatorBattery.java
@@ -0,0 +1,11 @@
+package com.braincadet.annot;
+
+import ij.plugin.PlugIn;
+
+public class AnnotatorBattery implements PlugIn {
+    @Override
+    public void run(String s) {
+        System.out.println("AnnotatorBattery");
+
+    }
+}
